5b13a8678c825548eadcf0ff5f46ae27c4d7dd0b
Jack-Thornhill <phisherman10@hotmail.com>
2020-05-01 13:13:51 -0500

    homepage

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,26 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>Linux Installation Guide</h1>
-    <h1>Hello, and welcome to Jack's guide to surviving FSA with a PC</h1>
-    <div>
-      <p>
-        In this guide I'm going to layout a walkthrough for using either a Linux
-        or Windows OS and the steps that you'll take to get your machine up and
-        running with vscode, postgresql, slack, chrome, zoom, npm/node and more.
-        If you're coming into FSA with a PC your first inclination may be to use
-        Windows. However, I would HIGHLY recommend opting to use a Linux
-        operating system instead. The setup may look like it will take longer at
-        the beginning, but a lot of what you'll need for coding will already be
-        included on the Linux installation, and really, the installation, along
-        with all the downloaded applications you'll need will be on Linux. If
-        you still want to use Windows, recognize that you'll need to use a tool
-        called WSL, or Windows Subsystem for Linux, that is essentially running
-        a version of Linux on your Windows machine, that will allow you access
-        to UNIX commands that will be similar to what your classmates are using
-        on their macbooks, so for the most part you'll be able to follow along.
-      </p>
-    </div>
+    <h1>Linux/Windows @ FSA Installation Guide</h1>
     <nav>
       {isLoggedIn ? (
         <div>
@@ -38,16 +19,11 @@ const Navbar = ({handleClick, isLoggedIn}) => (
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
-          <Link to="/home">Home</Link>
+
           <div>
-            <Link to="/linuxStart">
-              <h5>
-                Click Here to Follow Linux Installation Guide (Also, make sure
-                to have a usb with 8gb of memory)
-              </h5>
-            </Link>
+            <Link to="/linuxStart">The Linux Installation Guide</Link>
           </div>
-          <Link to="/login">Login</Link>
+          <Link to="/home">Home</Link>
           <Link to="/signup">Sign Up</Link>
         </div>
       )}
--- client/components/user-home.js
+++ client/components/user-home.js
@@ -5,12 +5,29 @@ import {connect} from 'react-redux'
 /**
  * COMPONENT
  */
-export const UserHome = props => {
-  const {email} = props
-
+export const UserHome = () => {
   return (
     <div>
-      <h3>Welcome, {email}</h3>
+      <h1>Hello, and welcome to Jack's guide to surviving FSA with a PC</h1>
+      <div>
+        <p>
+          In this guide I'm going to layout a walkthrough for using either a
+          Linux or Windows OS and the steps that you'll take to get your machine
+          up and running with vscode, postgresql, slack, chrome, zoom, npm/node
+          and more. If you're coming into FSA with a PC your first inclination
+          may be to use Windows. However, I would HIGHLY recommend opting to use
+          a Linux operating system instead. The setup may look like it will take
+          longer at the beginning, but a lot of what you'll need for coding will
+          already be included on the Linux installation, and really, the
+          installation, along with all the downloaded applications you'll need
+          will be on Linux. If you still want to use Windows, recognize that
+          you'll need to use a tool called WSL, or Windows Subsystem for Linux,
+          that is essentially running a version of Linux on your Windows
+          machine, that will allow you access to UNIX commands that will be
+          similar to what your classmates are using on their macbooks, so for
+          the most part you'll be able to follow along.
+        </p>
+      </div>
     </div>
   )
 }
