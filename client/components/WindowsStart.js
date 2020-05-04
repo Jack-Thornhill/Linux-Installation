3de66fb4561a65366400703aafdd2c0ac0d8ac22
Jack-Thornhill <phisherman10@hotmail.com>
2020-05-04 00:26:58 -0500

    style

--- client/components/WindowsStart.js
+++ client/components/WindowsStart.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 import {styled} from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
+import {TableCell} from '@material-ui/core'
 
 const MyStyle = styled(Button)({
   background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
@@ -13,7 +13,7 @@ const MyStyle = styled(Button)({
   padding: '0 30px'
 })
 
-const Warning = styled(Button)({
+const Warning = styled(TableCell)({
   background: 'red',
   border: 0,
   borderRadius: 3,
